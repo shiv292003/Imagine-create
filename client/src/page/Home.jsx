58d0fb800bafcbda52c00f7fcb461959452e5661
Shiv Shakti Sharma <90518426+shiv292003@users.noreply.github.com>
2023-08-08 14:51:28 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://long-pear-hatchling-yoke.cyclic.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
